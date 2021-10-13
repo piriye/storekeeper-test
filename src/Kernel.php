d447cde7d48d57e7f816b82be51d08a2bc7f3d1d
luki <szymon.lukaszczyk@gmail.com>
2021-10-13 15:39:05 +0200

    Added example db connection

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,13 +10,37 @@ class Kernel
 {
     public function handleRequest(Request $request): Response
     {
+        $pdo = $this->getDbConnection();
+        $now = $this->getDbNow($pdo);
         $response = new JsonResponse([
             'title' => 'Assessment from api',
             'time' => time(),
+            'dbtime' => $now,
             'path' => $request->getPathInfo(),
         ]);
         $response->headers->set('Access-Control-Allow-Origin', '*');
 
         return $response;
+    }
+
+    protected function getDbConnection(): \PDO
+    {
+        $dsn = getenv('DATABASE_DNS');
+        $user = getenv('DATABASE_USER');
+        $password = getenv('DATABASE_PASSWORD');
+        $pdo = new \PDO($dsn, $user, $password, [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+        ]);
+
+        return $pdo;
+    }
+
+    protected function getDbNow(\PDO $pdo): string
+    {
+        $statement = $pdo->prepare('SELECT now();');
+        $statement->execute();
+        $now = $statement->fetchColumn(0);
+
+        return $now;
     }
 }
