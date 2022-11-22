ad702910f465c13bbfed377c68de0875c4aff84b
kaladakienka <kaladakienka@gmail.com>
2022-11-22 13:15:26 +0100

    add migrations for orders, items and order_items

---
+++ db/migrations/20221122104520_item.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Item extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $items = $this->table('items');
+
+        $items
+            ->addColumn('name', 'string', ['limit' => 45, 'null' => false])
+            ->addColumn('price', 'decimal', ['precision' => 4, 'scale' => 3])
+            ->addColumn('created_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP']);
+
+        $items->create();
+    }
+}
---
+++ db/migrations/20221122104539_order.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class Order extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $items = $this->table('orders');
+
+        $items
+            ->addColumn('order_number', 'string', ['limit' => 45, 'null' => false])
+            ->addColumn('total_price', 'decimal', ['precision' => 4, 'scale' => 3])
+            ->addColumn('created_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP']);
+
+        $items->create();
+
+        $refTable = $this->table('order_items');
+        $refTable
+            ->addColumn('order_id', 'integer', ['limit' => 45, 'null' => false])
+            ->addColumn('item_id', 'integer', ['limit' => 45, 'null' => false])
+
+            ->addForeignKey('order_id', 'orders', 'id', ['delete'=> 'SET_NULL', 'update'=> 'NO_ACTION'])
+            ->addForeignKey('item_id', 'items', 'id', ['delete'=> 'SET_NULL', 'update'=> 'NO_ACTION'])
+
+            ->addColumn('created_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP'])
+            ->addColumn('updated_at', 'timestamp', ['null' => false, 'default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP']);
+        
+        $refTable->create();
+    }
+}
